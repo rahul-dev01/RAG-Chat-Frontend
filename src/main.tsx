467dbf05b3d22aac6c920c4185f12f8e77cd8742
Rahul <rajrahul00945@gmail.com>
2025-08-29 16:05:41 +0530

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,6 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/chat/:uuid" element={<Chat />} />
-          {/* <Route path="*" element={<div>404 - Page Not Found</div>} /> */}
         </Routes>
       </Router>
       <Toaster />
